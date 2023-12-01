8c90b1eb8bca37038259df9aa8d9df784eb3f921
vmoutteau <valentin.moutteau@gmail.com>
2023-12-01 13:30:37 +0100

    Traitement des arguments de HdfsServer.java + checkstyle

--- src/config/Config.java
+++ src/config/Config.java
@@ -10,21 +10,22 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Config implements Iterable<String> {
-    
+
     private static final String CONFIG_FOLDER = "config";
-    private static final String CONFIG_FILE = "config1"; 
+    private static final String CONFIG_FILE = "config1";
+    private static final String PATH = CONFIG_FOLDER + File.separator + CONFIG_FILE;
 
     private List<String> machines;
 
     public Config() {
-        machines = new ArrayList<>(); 
-        try (BufferedReader br = new BufferedReader(new FileReader(CONFIG_FOLDER + File.separator + CONFIG_FILE))) {
+        machines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(PATH))) {
             String line;
             while ((line = br.readLine()) != null) {
                 machines.add(line);
             }
         } catch (FileNotFoundException e) {
-            System.err.println("Unable to find config file :" + CONFIG_FOLDER + File.separator + CONFIG_FILE);
+            System.err.println("Unable to find config file :" + PATH);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/hdfs/HdfsServer.java
+++ src/hdfs/HdfsServer.java
@@ -4,28 +4,57 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class HdfsServer implements Runnable{
+/** Serveur Hdfs, dont plusieurs instances seront lancés sur plusieurs machines
+différentes. */
+public class HdfsServer {
 
-    private Socket socket;
-    public HdfsServer(Socket s) {
-        this.socket = s;
-    }
-    
+    /** Lancer un serveur HDFS.
+     * @param args : les arguments en ligne de commande, qui doit ne contenir qu'un
+     * argument.
+    */
     public static void main(String[] args) {
-        int port = 0; //TODO : gérer les arguments 
-        
-        try (ServerSocket ss = new ServerSocket(port)) {
-            while (true) {
-                Socket s = ss.accept();
-                new Thread(new HdfsServer(s)).start();
+        if (args.length != 1) {
+            usage();
+        } else {
+            int port = Integer.parseInt(args[0]); //
+            try (ServerSocket ss = new ServerSocket(port)) {
+                while (true) {
+                    Slave sl = new Slave(ss.accept());
+                    sl.start();
+                }
+            } catch (IOException e) {
+                System.err.println("Unable to launch Server :\n" + e.getMessage());
+            } catch (NumberFormatException e) {
+                usage();
             }
-        } catch (IOException e) {
-            System.err.println("Unable to launch Server :\n" + e.getMessage());
         }
     }
 
-    @Override
-    public void run() {
-        // TODO : todo
+
+    /* Usage de la classe HdfsServer. */
+    private static void usage() {
+        System.out.println("Usage : java HdfsServer <PortNumber>");
     }
+
+    /* Esclave du socket server, thread qui sera lancé plusieurs fois par la classe
+     * HdfsServer.
+    */
+    private static class Slave extends Thread {
+
+        /** Socket accepté par le serveur. */
+        private Socket s;
+
+        /* Construire  un esclave à partir d'un socket accepté par le serveur.
+         * @param s : socket accepté par le serveur.
+        */
+        Slave(Socket s) {
+            this.s = s;
+        }
+
+        @Override
+        public void run() {
+            // TODO unimplemented method stub
+        }
+    }
+
 }
