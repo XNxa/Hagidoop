ab371d487c46e5f07ceb623e1b86d0db11e8ae96
XNxa <xaviernaxara@yahoo.fr>
2024-01-14 16:38:56 +0100

    Refactor KVNetworkReaderWriter class to support server-client communication

--- src/io/KVNetworkReaderWriter.java
+++ src/io/KVNetworkReaderWriter.java
@@ -1,59 +1,141 @@
 package io;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import interfaces.KV;
 import interfaces.NetworkReaderWriter;
 
 public class KVNetworkReaderWriter implements NetworkReaderWriter {
 
-    private String fname;
+    private String ip;
+    private int serverPort;
+    private ServerSocket ss;
+    private Socket cs;
 
-    public KVNetworkReaderWriter(String fname) {
-        this.fname = fname;
+    private enum Opentype {
+        SERVER, 
+        CLIENT
+    }
+
+    private Opentype opentype;
+    
+
+    public KVNetworkReaderWriter(int serverPort) {
+        this.serverPort = serverPort;
+        opentype = Opentype.SERVER; 
+    }
+
+    public KVNetworkReaderWriter(Socket s) {
+        this.cs = s;
+        opentype = Opentype.CLIENT;
+    }
+
+    public KVNetworkReaderWriter(String ip, int port) {
+        this.ip = ip;
+        this.serverPort = port;
+        opentype = Opentype.CLIENT;
     }
 
     @Override
     public KV read() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'read'");
+        if (opentype == Opentype.CLIENT) {
+            try (ObjectInputStream is = new ObjectInputStream(cs.getInputStream())) {
+                KV kv =  (KV) is.readObject();
+                return kv;
+            } catch (ClassNotFoundException | IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        } else {
+            throw new UnsupportedOperationException("You can't use the method read on a server.");
+        }
     }
 
     @Override
     public void write(KV record) {
-        System.out.println(record);
+        if (opentype == Opentype.CLIENT) {
+            try (ObjectOutputStream os = new ObjectOutputStream(cs.getOutputStream())) {
+                os.writeObject(record);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        } else {
+            throw new UnsupportedOperationException("You can't use the method write on a server.");
+        }
     }
 
     @Override
     public void openServer() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'openServer'");
+        if (opentype == Opentype.SERVER) { 
+            try {
+                ss = new ServerSocket(serverPort);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } else {
+            throw new UnsupportedOperationException("You can't use the method openServer on a client.");
+        }
     }
 
     @Override
     public void openClient() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'openClient'");
+        if (opentype == Opentype.CLIENT) {
+            try {
+                cs = new Socket(this.ip, this.serverPort);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } else {
+            throw new UnsupportedOperationException("You can't use the method openClient on a server.");
+        }
     }
 
     @Override
     public NetworkReaderWriter accept() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accept'");
+        if (opentype == Opentype.SERVER) { 
+            try {
+                return new KVNetworkReaderWriter(ss.accept());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        } else {
+            throw new UnsupportedOperationException("You can't use the method accept on a client.");
+        }
     }
 
     @Override
     public void closeServer() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'closeServer'");
+        if (opentype == Opentype.SERVER) {
+            try {
+                ss.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } else { 
+            throw new UnsupportedOperationException("You can't use the method closeServer on a client.");
+        }
     }
 
     @Override
     public void closeClient() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'closeClient'");
-    }
-    
+        if (opentype == Opentype.CLIENT) {
+            try {
+                cs.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } else {
+            throw new UnsupportedOperationException("You can't use the method closeClient on a client.");
+        }
+    }    
 }
